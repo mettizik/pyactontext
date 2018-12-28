fe65ffc37bbbcb54e0fc7b4fda941ad59b0a4fc4
Mokych Andrey <mokych.andrey@apriorit.com>
2018-12-28 15:42:32 +0200

    Fixed OS version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
       'Programming Language :: Python :: 3.5',
       'Programming Language :: Python :: 3.6',
       'Programming Language :: Python :: 3.7',      
-      "Operating System :: Windows",
+      "OPERATING SYSTEM :: MICROSOFT :: WINDOWS",
     ],
     ext_modules=[
         Extension('actontext_internal', ['native/actonext.cpp'])],
